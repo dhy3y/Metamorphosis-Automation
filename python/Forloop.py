d126c44e1dce58a01a6d6d0c8e4d0fdf64a3ee20
rks507469 <44531949+rks507469@users.noreply.github.com>
2020-10-08 18:23:02 +0530

    Python Files added

---
+++ python/Forloop.py
@@ -0,0 +1,4 @@
+#This is ho eto write for loop in python
+mylist = ['a','b','c','d','e']
+for x in mylist:
+    print(x)
---
+++ python/List.py
@@ -0,0 +1,8 @@
+#List in Python are like arrays
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append(4)
+mylist.append(5)
+print(mylist)
